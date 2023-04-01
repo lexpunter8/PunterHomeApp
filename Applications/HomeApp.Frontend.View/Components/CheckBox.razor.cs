79b96e3ab4fcc3d5a393b9ba768ada08db79bd15
lexpunter8 <lex.punter@gmail.com>
2023-04-01 12:54:44 +0200

    wip

---
+++ Applications/HomeApp.Frontend.View/Components/CheckBox.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace HomeApp.Frontend.View.Components
+{
+    public partial class CheckBox : ComponentBase
+    {
+        public bool IsChecked { get; set; }
+
+        [Parameter]
+        public EventCallback<bool> CheckedChanged { get; set; }
+
+
+        private void Cliked()
+        {
+            IsChecked = !IsChecked;
+            CheckedChanged.InvokeAsync(IsChecked);
+        }
+    }
+}
--- Applications/HomeApp.Frontend.View/Components/ShoppingListItemComponent.razor.cs
+++ Applications/HomeApp.Frontend.View/Components/ShoppingListItemComponent.razor.cs
@@ -4,5 +4,42 @@ namespace HomeApp.Frontend.View.Components
 {
     public partial class ShoppingListItemComponent : ComponentBase
     {
+        public bool IsEditting { get; set; }
+        public bool IsChecked { get; set; }
+
+        [Parameter]
+        public int Count { get; set; }
+
+        private void Edit()
+        {
+            if (IsChecked)
+            {
+                return;
+            }
+            IsEditting = true;
+        }
+
+        private void StopEdit()
+        {
+            IsEditting = false;
+        }
+
+        private void OnCheckedChanged(bool isChecked)
+        {
+            IsChecked = isChecked;
+            StopEdit();
+        }
+
+        private void IncreaseCount()
+        {
+            Count++;
+            StateHasChanged();
+        }
+
+        private void DecreaseCount()
+        {
+            Count--;
+            StateHasChanged();
+        }
     }
 }
